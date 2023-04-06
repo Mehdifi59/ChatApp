74b9a022f6c3b8ba63acd0c41abab5100db4681d
Mehdi Zarouri <m.zarouri59@hotmail.fr>
2023-04-06 17:17:37 +0200

    Ajout système d'émojis

--- js/index.js
+++ js/index.js
@@ -6,9 +6,26 @@ const btn_emoji = document.getElementById('btn_emoji');
 
 const chat_list = document.getElementById('chat_list');
 
+const emojispan = document.getElementById('emojispan');
 
 
 const emojis = ["👍","👌","👏","🙏","🆗","🙂","😀","😃","😉","😊","😋","😌","😏","😐","😑","😒","😓","😂","🤣","😅","😆","😜","😹","🚶","👫","👬","👭","😙","😘","🏠","👆","🖕","👋","👎","👈","👉"];
+
+const span = document.createElement('span');
+emojis.forEach((emoji) => {
+    const btn_for_emoji = document.createElement('button');
+    btn_for_emoji.textContent = emoji;
+    span.appendChild(btn_for_emoji);
+    emojispan.appendChild(span);
+    
+    btn_for_emoji.addEventListener('click', function(e) {
+        e.preventDefault();
+        const active_input = document.activeElement;
+        msg_user.value += emoji;
+        console.log(active_input)
+    })
+
+})
 
 
 
@@ -38,5 +55,8 @@ btn_send.onclick = function() {
         li.appendChild(document.createTextNode(date.getHours() + ':' +  date.getMinutes() + ':' + date.getSeconds()));
         chat_list.appendChild(li);    
     }
+
+    msg_user.value = "";
+    msg_client.value = "";
 }
 
